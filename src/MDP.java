f3fe4e7d4c25e565da7b55da9c53c7db271a29a5
Fernando Karchiloff <fernando.kga@gmail.com>
2020-05-21 09:08:06 -0300

    Reading files

--- src/MDP.java
+++ src/MDP.java
@@ -1,3 +1,4 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -8,19 +9,33 @@ public class MDP {
     static final String fixedGoalInitialState = "FixedGoalInitialState";
     static final String randomGoalInitialState = "RandomGoalInitialState";
     
+    // Program must be run from top level folder (tpia-ep2-mdp) with command: java src/MDP.java
     public static void main(String[] args) throws Exception {
-        // -f -> fixed - navigation_number
-        // -r -> random - navigation_number
-        // navigation_number goes from 1 to 10
-        
+        /**
+         * -f navigation_number
+         * -r navigation_number
+         * 
+         * -f -> fixed
+         * -r -> random
+         * navigation_number goes from 1 to 10
+         */
+
         FileReader file;
         switch(args[0]) {
             case "-f":
                 file = getFileReader(args[1], fixedGoalInitialState);
+                BufferedReader br = new BufferedReader(file);
+                while (br.ready()) {
+                    System.out.println(br.readLine());
+                }
                 file.close();
                 break;
             case "-r":
                 file = getFileReader(args[1], randomGoalInitialState);
+                BufferedReader br = new BufferedReader(file);
+                while (br.ready()) {
+                    System.out.println(br.readLine());
+                }
                 file.close();
                 break;
             default:
@@ -30,6 +45,6 @@ public class MDP {
 
     public static FileReader getFileReader(String fileNumber, String folder) throws FileNotFoundException{
         String absolutePath = new File("").getAbsolutePath();
-        return new FileReader(absolutePath+"/../files/"+folder+"/"+file_prefix+fileNumber+file_format);
+        return new FileReader(absolutePath+"/files/"+folder+"/"+file_prefix+fileNumber+file_format);
     }
 }
